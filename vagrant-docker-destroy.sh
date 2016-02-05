47668b6cd8954e3b2787850b067e6d84b479acf5
RedNacho <RedNacho@bitbucket.com>
2016-02-05 16:33:58 +0000

    Passing through -f argument to docker-machine rm and vagrant destroy.

--- vagrant-docker-destroy.sh
+++ vagrant-docker-destroy.sh
@@ -3,10 +3,11 @@
 #Parameters:
 # - The name to give the docker-machine (will be destroyed if it already exists)
 # - The location of the Vagrantfile for the vagrant box
+# - (Optional) "-f" to force destruction
 
 #Remove the docker machine
-docker-machine rm "$1"
+docker-machine rm $3 "$1"
 
 #Destroy the vagrant box
 cd "$2"
-vagrant destroy
+vagrant destroy $3
