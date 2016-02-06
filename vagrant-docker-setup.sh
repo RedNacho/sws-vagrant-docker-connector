4b346a579a49631429424f1b762f4fdade5f365e
RedNacho <RedNacho@bitbucket.com>
2016-02-06 20:59:53 +0000

    Adding option to use vagrant SSH config for IP and port.

--- vagrant-docker-setup.sh
+++ vagrant-docker-setup.sh
@@ -6,6 +6,9 @@
 # - (Optional) The network adapter on the vagrant box from which we can acquire
 #   an IP address visible to the host (the vagrant box must be configured so
 #   that this is possible, e.g. with a private network). If omitted, eth1.
+#   Set this to --vagrant-ssh to rely on the vagrant ssh config for the IP
+#   address and SSH port instead (this may require docker ports to be forwarded
+#   to the host IP).
 
 #Change to the vagrant directory
 cd "$2"
@@ -25,11 +28,24 @@ vagrant_user=$(echo "${sshconfig}" | grep "^\s*User\s" | sed -e "s/^\s*User //")
 #Strip out quote marks from SSH key path (otherwise it doesn't work...)
 vagrant_ssh_key=$(echo "${vagrant_ssh_key}" | sed -e "s/\"//g")
 
+if [ "$3" == "--vagrant-ssh" ]
+then
+
+#Get the IP and SSH port of the vagrant box from the vagrant ssh-config. This
+#may be the host IP and a forwarded port, in which case docker ports must
+#also be forwarded.
+
+vagrant_ip=$(echo "${sshconfig}" | grep "^\s*HostName\s" | sed -e "s/^\s*HostName //")
+vagrant_ssh_port=$(echo "${sshconfig}" | grep "^\s*Port\s" | sed -e "s/^\s*Port //")
+
+else
+
 #Pull out the direct IP and SSH port of the vagrant box. (We can't use the
 #host port mapping, because docker-machine will try to communicate with
 #the docker host on port 2376. We don't want to require that this port is
 #forwarded from the vagrant box, as we can only do this for one docker host
 #at a time.)
+
 vagrant_direct_ssh=$(vagrant ssh -c "ip address show ${3:-eth1} | grep 'inet ' | sed -e 's/^.*inet /ip=/' -e 's/\/.*$//' && grep Port /etc/ssh/sshd_config | sed -e 's/Port /port=/'")
 
 #Get rid of spurious carriage returns...
@@ -37,6 +53,8 @@ vagrant_direct_ssh=$(echo "${vagrant_direct_ssh}" | sed -e "s/\r//")
 
 vagrant_ip=$(echo "${vagrant_direct_ssh}" | grep "^ip=" | sed -e "s/^ip=//")
 vagrant_ssh_port=$(echo "${vagrant_direct_ssh}" | grep "^port=" | sed -e "s/^port=//")
+
+fi
 
 #Remove existing docker-machine
 docker-machine rm -f "$1"
